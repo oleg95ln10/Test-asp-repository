909a5e93ffd80f19244fd5ec0d8f1c970b8c8fc2
Oleg <Oleg@OLEG_PC>
2016-09-27 01:35:14 +0300

    Changed products

--- WebApplication4 test/Models/Product.cs
+++ WebApplication4 test/Models/Product.cs
@@ -8,10 +8,20 @@ namespace WebApplication4_test.Models
     public class Product
     {
         private string name;
+        public int ProductID { get; set; }
         public string Name
         {
-            get { return name; }
-            set { name = value; }
+            get
+            {
+                return ProductID + name;
+            }
+            set
+            {
+                name = value;
+            }
         }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { set; get; }
     }
 }
