a162d3f894aae8a0065bd7245d7b11cf9aead1a6
Oleg <Oleg@OLEG_PC>
2016-09-27 01:57:18 +0300

    added use extention

--- WebApplication4 test/Controllers/HomeController.cs
+++ WebApplication4 test/Controllers/HomeController.cs
@@ -46,5 +46,22 @@ namespace WebApplication4_test.Controllers
             };
             return View("Result", (object)stringArray[1]);
         }
+        public ViewResult UseExtension()
+        {
+            // создание и заполнение ShoppingCart
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product> {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+            // получение общей стоимости продуктов в корзине
+            decimal cartTotal = cart.TotalPrices();
+            return View("Result",
+            (object)String.Format("Total: {0:c}", cartTotal));
+        }
     }
 }
