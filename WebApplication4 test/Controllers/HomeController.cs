ff4e9ec06e8e468b462707843c2a99b9b6c4a20a
Oleg <Oleg@OLEG_PC>
2016-09-27 01:30:55 +0300

    Added autoproperty

--- WebApplication4 test/Controllers/HomeController.cs
+++ WebApplication4 test/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication4_test.Models;
 
 namespace WebApplication4_test.Controllers
 {
@@ -12,6 +13,17 @@ namespace WebApplication4_test.Controllers
         {
             return "Navigate to a URL to show an example";
         }
-
+        public ViewResult AutoProperty()
+        {
+            // создается новый объект Product
+            Product myProduct = new Product();
+            // устанавливается значение свойства
+            myProduct.Name = "Kayak";
+            // читается свойство
+            string productName = myProduct.Name;
+            // генерируется представление
+            return View("Result",
+            (object)String.Format("Product name: {0}", productName));
+        }
     }
 }
