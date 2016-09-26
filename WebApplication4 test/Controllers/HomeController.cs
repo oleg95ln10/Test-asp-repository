e2570f62366826687890b12c4044ea2acdf1d5ae
Oleg <Oleg@OLEG_PC>
2016-09-27 01:28:34 +0300

    Added few classes

--- WebApplication4 test/Controllers/HomeController.cs
+++ WebApplication4 test/Controllers/HomeController.cs
@@ -13,18 +13,5 @@ namespace WebApplication4_test.Controllers
             return "Navigate to a URL to show an example";
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ WebApplication4 test/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4_test.Models
+{
+    public class Product
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
